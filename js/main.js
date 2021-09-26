a6460fce42fce7a66636e49b47f8affb076fb234
williams.parra.parra@gmail.com <williams.parra.parra@gmail.com>
2021-09-26 15:37:22 -0300

    Implementacion de carga de personajes
    -se utiliza fetch para realizar peticion a la api
    -se modifican stilos de css
    -se modifican botones de carga

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 //capturando ubicacion de destino
 const contenido = document.querySelector(".contenido");
+const btn_siguiente = document.getElementById("cargarMas");
+const ruta_Api ="https://rickandmortyapi.com/api/character";
+let numeroMax;
+let carga=1;
+let ruta_Siguiente;
+
+
 
 function dibujarPersonaje(ob){  
     //creando elementos html a modificar
@@ -14,10 +21,20 @@ function dibujarPersonaje(ob){
     const gender = document.createElement("p");
     const genderR = document.createElement("p");
     //modificando elementos html
-    nombre.textContent = ob.nombre;
-    img.src = ob.imagen;
+    nombre.textContent = ob.name;
+    img.src = ob.image;
     status.textContent = "status:";
     statusR.textContent = ob.status;
+    if(ob.status==="Alive"){
+        statusR.className="alive";
+    }
+    if(ob.status==="Dead"){
+        statusR.className="dead";
+    }
+    if(ob.status==="unknown"){
+        statusR.className="unknown";
+    }
+    
     species.textContent ="species:";
     speciesR.textContent =ob.species;
     gender.textContent ="gender:";
@@ -40,6 +57,45 @@ function dibujarPersonaje(ob){
     console.log("agregado!");
 }
 
+function limpiarContenido(){
+    contenido.innerHTML = "";
+}
+
+//falta terminar de implementar
+function cargarApi(ruta=ruta_Api){
+    fetch(ruta)
+    .then(response=> response.json())
+    .then(data =>{
+        //rescatar siguiente direccion
+        ruta_Siguiente = data.info.next;
+        numeroMax= data.info.pages;
+        //dibujar-personajes
+        for (i of data.results){
+            dibujarPersonaje(i);
+        }
+    })
+    .catch(error =>{
+        const mensaje = document.createElement("h3");
+        mensaje.textContent="Error al cargar los datos.";
+        limpiarContenido();
+        contenido.appendChild(mensaje)        
+    })
+}
+
+btn_siguiente.addEventListener('click',()=>{
+    if(carga < numeroMax){
+        console.log("boton siguiente precionado")
+        cargarApi(ruta_Siguiente);
+        carga++;
+    }else{
+        btn_siguiente.disabled = true;
+        btn_siguiente.textContent = "No quedan mas personajes";
+    }
+    
+    
+})
 window.onload = function(){
-    console.log("termino de cargar la pagina");
+    console.log("termino de cargar la pagina"); 
+    limpiarContenido();   
+    cargarApi();
 }
